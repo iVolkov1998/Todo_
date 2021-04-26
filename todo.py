cade734974b50685926e410a7e7ca6104ba16f85
Zigfrid <>
2021-04-26 15:41:56 +0000

    Добавили функцию проверки даты.

--- todo.py
+++ todo.py
@@ -1,11 +1,27 @@
+import time
+
+
+
 HELP = """
 add - добавить задачу
 show - показать задачу
 done - убрать выполненую задачу
 exit - закрытть приложение
-
 """
+
+
 todo = {}
+
+def checkDate(date):
+  try:
+    time.strptime(date, "%d.%m.%Y")
+    return True
+  except ValueError:
+    print("Error. Не правильный формат даты")
+    return False
+
+
+
 print("Введите команду , введите help для вывода списка команд")
 
 while True:
@@ -14,8 +30,13 @@ while True:
   if userAnswer == "add":
     userDate = input("Введите дату:\n")
     userTask = input("Что нужно сделать?")
-    todo[userDate] = userTask
-    print(f"[{userDate}] - добавлена задача '{userTask}' " )
+
+  
+    if userDate in todo.keys():
+     todo[ userDate ].append(userTask)
+    else: 
+       todo[ userDate ] = [userTask]
+    print(f"[{userDate}] - добавлена задача '{userTask}'")
   elif userAnswer == "help":
     print(HELP)
   elif userAnswer == "exit":
@@ -23,6 +44,8 @@ while True:
   elif userAnswer == "done":
     print("Работает")
   elif userAnswer =="show":
-    for date in todo.keys():
-     print(f"[{date}] - \t {todo [date] }")
-
+   for date in sorted(todo.keys()):
+     for tasks in todo [ date ]:
+      print(f"[{date}] -  {tasks} ")
+  print ("Работает")
+  
